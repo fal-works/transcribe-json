a6d219125d36d0a35145027d3d511e6597aff923
FAL <falworks.contact@gmail.com>
2021-01-31 17:44:41 +0900

    docs: add comment

--- src/read.ts
+++ src/read.ts
@@ -4,6 +4,8 @@ const parseBuffer = (buffer: Buffer) =>
   JSON.parse(buffer.toString()) as Record<string, unknown>;
 
 /**
+ * Reads a JSON file and returns the parsed object.
+ *
  * @param srcfile Filepath to any JSON file.
  */
 export const read = (srcfile: string): Promise<Record<string, unknown>> =>
